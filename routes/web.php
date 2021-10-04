1e77030d0d8e1e8ed066ee5ad479964209745afd
nanopxl <panjirusdi@gmail.com>
2021-10-04 15:24:49 +0700

    add auth

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::post('inputform', 'App\Http\Controllers\webtestController@store');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
